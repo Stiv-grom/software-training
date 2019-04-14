f3e2b89b19d5d02a5338b5b129ba2f8b2e35ee2c
Stivgrom <stanislav.kostenko.mail@gmail.com>
2019-04-14 19:33:56 +0300

    hw3: union and unionall implementation

---
+++ HW3-UnionImplementation/HW3-UnionImplementation/ApartmentComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_UnionImplementation
+{
+    public class ApartmentComparer : IEqualityComparer<Apartment>
+    {
+        public bool Equals(Apartment x, Apartment y)
+        {
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the apartments' properties are equal.
+            return x.Id == y.Id && x.Name == y.Name && x.Country == y.Country && x.Latitude == y.Latitude &&
+                x.Location == y.Location && x.Longitude == y.Longitude && x.ZipCode == y.ZipCode;
+        }
+
+        // If Equals() returns true for a pair of objects 
+        // then GetHashCode() must return the same value for these objects.
+        public int GetHashCode(Apartment apartment)
+        {
+            if (Object.ReferenceEquals(apartment, null)) return 0;
+
+            int hash = 17;
+            hash = hash * 23 + apartment.Id.GetHashCode();
+            hash = hash * 23 + (apartment.Name ?? "").GetHashCode();
+            hash = hash * 23 + (apartment.Country ?? "").GetHashCode();
+            hash = hash * 23 + apartment.Latitude.GetHashCode();
+            hash = hash * 23 + (apartment.Location ?? "").GetHashCode();
+            hash = hash * 23 + apartment.Longitude.GetHashCode();
+            hash = hash * 23 + (apartment.ZipCode ?? "").GetHashCode();
+            return hash;
+
+        }
+    }
+}
---
+++ HW3-UnionImplementation/HW3-UnionImplementation/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_UnionImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HW3: UnionAll and Union for 2 entities with apartment");
+
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string barcelonaFirstFilePath = Path.Combine(projectDirectory, "Barcelona1.csv");
+            string barcelonaSecondFilePath = Path.Combine(projectDirectory, "Barcelona2.csv");
+
+            List<Apartment> apartments = ParseCsv(barcelonaFirstFilePath, false);
+            List<Apartment> apartments2 = ParseCsv(barcelonaSecondFilePath, true);
+
+            //union all
+            var watch = Stopwatch.StartNew();
+            List<ApartmentResult> unionAllResult = apartments.Union(apartments2).Select(x => new ApartmentResult()
+            {
+                Name = x.Name,
+                Latitude = x.Latitude,
+                Longitude = x.Longitude
+            }).ToList();
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(@"UnionAll was done in {0}, results count: {1}", elapsedMs, unionAllResult.Count);
+
+            //use custom comparer for union
+            watch = Stopwatch.StartNew();
+            var customApartmentComparer = new ApartmentComparer();
+            List<ApartmentResult> unionResult = apartments.Union(apartments2, customApartmentComparer).Select(x => new ApartmentResult()
+            {
+                Name = x.Name,
+                Latitude = x.Latitude,
+                Longitude = x.Longitude
+            }).ToList();
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine(@"Union was done in {0}, results count: {1}", elapsedMs, unionResult.Count);
+
+            Console.WriteLine("Completed");
+        }
+
+        public static List<Apartment> ParseCsv(string csvFilePath, bool parseId)
+        {
+            List<Apartment> apartments = new List<Apartment>();
+            using (TextFieldParser parser = new TextFieldParser(csvFilePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                bool firstLine = true;
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+
+                    if (firstLine)
+                    {
+                        firstLine = false;
+                        continue;
+                    }
+                    apartments.Add(new Apartment(fields, parseId));
+                }
+            }
+            return apartments;
+        }
+    }
+}
